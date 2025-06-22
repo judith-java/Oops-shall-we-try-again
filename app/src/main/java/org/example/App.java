b5a82c844c36f4f585b9e9dd716e8b5c833165ea
judith-java <mi97234@mail.harpercollege.edu>
2025-06-22 22:01:39 +0000

    Changes to be committed: modified:   app/src/main/java/org/example/App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public int getInput() {
-    // Implement your function here
+
+  static int lowerBound = 10;
+  static int upperBound = 100;
+  static String prompt = "Please enter an integer between 10 and 100:";
+  static String errorMessage = "Your value is invalid. Please try again.";
+
+  public static int getInput() {
+    Scanner scanner = new Scanner(System.in);
+    int userInput;
+
+    while (true) {
+      System.out.println(prompt);
+      if (scanner.hasNextInt()) {
+        userInput = scanner.nextInt();
+        if (userInput >= lowerBound && userInput <= upperBound) {
+          return userInput;
+        } else {
+          System.out.println(errorMessage);
+        }
+      } else {
+        scanner.next(); 
+        System.out.println(errorMessage);
+      }
+    }
   }
 
   public static void main(String[] args) {
-    // Test your function here in a driver program
+    int result = getInput();
+    System.out.println("The value chosen by the user is " + result);
   }
 }
